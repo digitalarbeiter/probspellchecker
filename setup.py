1f8691553e6dd5134153598a5e0864882d98a159
Patrick Schemitz <ps@solute.de>
2018-09-03 15:49:53 +0200

    bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md") as f:
 
 setup(
     name="probspellchecker",
-    version="0.1.0",
+    version="0.1.1",
     description="probabilistic spell checker",
     license="LGPLv3",
     long_description=long_description,
